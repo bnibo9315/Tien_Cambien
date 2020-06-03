b90b0aa4ea02ac0c8d767050f354b88b89d5182d
bnibo9315 <ngumahobao@gmail.com>
2020-06-03 07:02:24 +0700

    Update APK Tien v3.0

--- CamBienNhietDo/CambienNhietDo/CambienNhietDo.ino
+++ CamBienNhietDo/CambienNhietDo/CambienNhietDo.ino
@@ -1,3 +1,11 @@
+////////////////////////////////////////////////////
+//  Author : Thanh Quang Long - Founder HNLShop   //
+//  Day : 1/6/2020                                //
+////////////////////////////////////////////////////
+//Notice Bluetooth:
+// 1 : ON
+// 2 : OFF
+#include <SoftwareSerial.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
@@ -7,13 +15,20 @@
 OneWire oneWire(DS18B20);
 DallasTemperature sensors(&oneWire);
 LiquidCrystal_I2C lcd(0x27,20,2); 
-int temp ;
+SoftwareSerial bluetooth(0,1);
+float temp ;
+char state;
+int pwm;
+
+String nhietdo;
 #define Motor12V1  6
 #define Motor12V2  5
 
 String intro = "MSSV : 1653020083 ";
 void setup()
 {
+  bluetooth.begin(9600); 
+  Serial.begin(9600);
   pinMode(Motor12V1, OUTPUT);
   pinMode(Motor12V2, OUTPUT);
   lcd.init();                    
@@ -35,16 +50,21 @@ void setup()
 void loop()
 {
   sensors.requestTemperatures();  
+   
 //  MotorfollowTemp();
-  Motor6Lv();
-  delay(50);
+//  Motor6Lv();
+  displaysreen();
+  bluetoothDisplay();
+  
 }
+
 void MotorControllerON(int PWM)
 {
   digitalWrite(Motor12V1,LOW);
   analogWrite(Motor12V2, PWM); 
   delay(500);
-  }void MotorControllerOFF()
+  }
+void MotorControllerOFF()
 {
   digitalWrite(Motor12V1,LOW);
   digitalWrite(Motor12V2, LOW); 
@@ -59,6 +79,19 @@ void displaysreen()
   lcd.print(sensors.getTempCByIndex(0));
   lcd.setCursor(0,1);
   lcd.print("Lv:");
+  }
+void displaysreenOFF()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("NhietDo:");
+  lcd.setCursor(10,0);
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.setCursor(0,1);
+  lcd.print("Lv: OFF");
+      Serial.print(sensors.getTempCByIndex(0));
+    Serial.print("                         OFF ");
+    Serial.print("\n");
   }
 void MotorfollowTemp()
 {
@@ -101,15 +134,21 @@ void Motor6Lv()
     displaysreen();
     lcd.setCursor(4,1);
     lcd.print("Off");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print("                           OFF ");
+    Serial.print("\n");
     }
   else if(15 <= sensors.getTempCByIndex(0) && sensors.getTempCByIndex(0) < 30)
   {
-    MotorControllerON(65);
+    MotorControllerON(75);
     displaysreen();
     lcd.setCursor(4,1);
     lcd.print("1");
     lcd.setCursor(6,1);
     lcd.print("Duty: 25%");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print(  "                          1 ");
+    Serial.print("\n");
   }
   else if (30 <= sensors.getTempCByIndex(0) && sensors.getTempCByIndex(0) < 35)
   {
@@ -119,6 +158,9 @@ void Motor6Lv()
     lcd.print("2");
     lcd.setCursor(6,1);
     lcd.print("Duty: 35%");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print( "                          2 ");
+    Serial.print("\n");
     }
   else if (35 <= sensors.getTempCByIndex(0) && sensors.getTempCByIndex(0) < 45)
   {
@@ -128,6 +170,9 @@ void Motor6Lv()
     lcd.print("3");
     lcd.setCursor(6,1);
     lcd.print("Duty: 50%");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print( "                         3 ");
+    Serial.print("\n");
     }
   else if (45 <= sensors.getTempCByIndex(0) && sensors.getTempCByIndex(0) < 60)
   {
@@ -137,6 +182,9 @@ void Motor6Lv()
     lcd.print("4");
     lcd.setCursor(6,1);
     lcd.print("Duty: 65%");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print( "                          4 ");
+    Serial.print("\n");
     }
     else if (60 <= sensors.getTempCByIndex(0) && sensors.getTempCByIndex(0) < 80)
   {
@@ -146,6 +194,9 @@ void Motor6Lv()
     lcd.print("5");
     lcd.setCursor(6,1);
     lcd.print("Duty: 80%");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print( "                        5 ");
+    Serial.print("\n");
     }
     else
   {
@@ -155,6 +206,37 @@ void Motor6Lv()
     lcd.print("6");
     lcd.setCursor(6,1);
     lcd.print("Duty: Max");
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print("                         6 ");
+    Serial.print("\n");
+  }
+}
+void bluetoothDisplay()
+{
+  if(bluetooth.available() > 0){
+    state = bluetooth.read();
+    int pwm = bluetooth.read();
+ MotorControllerON(pwm);
+  } 
+  switch (state) {
+  case '1':
+    displaysreenOFF();
+    MotorControllerOFF();
+     break;
+  case '2':
+     displaysreen();
+     MotorfollowTemp();
+     break;
+  case '3':
+    displaysreen();
+    Motor6Lv();
+    break;
+// case '4':
+//    MotorfollowTemp();
+//    break;
     
+  default:
+   MotorControllerOFF(); 
+   break;
 }
   }
